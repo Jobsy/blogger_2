0ad0e2b4aad22061a88f63bb08d5ebb0a87fc4e1
Oluwajoba <oluwajobabello@gmail.com>
2018-12-22 02:31:29 +0100

    form-based workflow feature completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
     def index
         @articles = Article.all
     end
-
     def show
         @article = Article.find(params[:id])
     end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(params[:article])
+        @article.save
+        redirect_to article_path(@article)
+    end
+    
 end
